a8b5ed794d803c58e984199dc4fbfff4134b3467
evikairoz <evisigalingging124@gmail.com>
2024-12-08 23:41:45 +0700

    5. Update Splashscreen 6. update About

--- app/src/main/java/com/example/projectuas1/splashscreen.java
+++ app/src/main/java/com/example/projectuas1/splashscreen.java
@@ -10,12 +10,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
+
 
 public class splashscreen extends AppCompatActivity {
+
+    ImageView splashimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,13 @@ public class splashscreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ImageView splashimage = findViewById(R.id.Imagesplash);
+
+        Glide.with(this)
+                .asGif()
+                .load(R.drawable.splash)  // File GIF di folder res/drawable
+                .into(splashimage);
+
         // to show splashscreen selama 3 detik (3000 ms)
         new Handler().postDelayed(new Runnable() {
             @Override
